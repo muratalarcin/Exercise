3b142baf4d7a9b839fe3c84ae665c5f9894634d2
Murat Alarcin <murattrbl@icloud.com>
2023-11-13 13:55:31 +0300

    second commit

--- app/src/main/java/com/muratalarcin/lalala/BirinciFragment.java
+++ app/src/main/java/com/muratalarcin/lalala/BirinciFragment.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.SearchView;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
@@ -26,6 +27,7 @@ import com.google.android.material.timepicker.TimeFormat;
 import com.muratalarcin.lalala.databinding.FragmentBirinciBinding;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class BirinciFragment extends Fragment {
     FragmentBirinciBinding binding;
@@ -38,14 +40,58 @@ public class BirinciFragment extends Fragment {
             Navigation.findNavController(view).navigate(R.id.action_birinciFragment_to_ikinciFragment);
         });
 
-        ArrayList<String> ulkeler = new ArrayList<>();
-        ulkeler.add("Türkiye");
-        ulkeler.add("İtalya");
-        ulkeler.add("Japonya");
+        String[] ulkeler = {"Türkiye", "Almanya", "Fransa", "İngiltere", "İtalya", "İspanya"};
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.simple_list_item, ulkeler);
-        binding.autoTextView.setAdapter(arrayAdapter);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, ulkeler);
+        binding.autoTextView.setAdapter(adapter);
+
+        binding.floatingActionButton.setOnClickListener(view -> {
+            String lala = binding.autoTextView.getText().toString();
+            binding.textView2.setText(lala);
+        });
+
+        binding.ratingBar.setOnRatingBarChangeListener(((ratingBar, rating, userChanged) -> {
+            if (userChanged) {
+                binding.textView2.setText(String.valueOf(rating));
+            }
+        }));
+
+
+        binding.buttonDate.setOnClickListener(view -> {
+            MaterialDatePicker dp = MaterialDatePicker.Builder.datePicker()
+                    .setTitleText("Tarih Seç")
+                    .build();
+
+            dp.show(getChildFragmentManager(), "");
+
+            dp.addOnPositiveButtonClickListener(view1 -> {
+                SimpleDateFormat df = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss", Locale.getDefault());
+                String tarih = df.format(view1);
+                binding.editTextDate.setText(tarih);
+            });
+        });
+
+        binding.searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                ara(s);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                ara(s);
+                return true;
+            }
+        });
+
+
+
+
 
         return binding.getRoot();
+    }
+    public void ara(String araKelimesi) {
+        Log.e("Kişi Ara", araKelimesi);
     }
 }
